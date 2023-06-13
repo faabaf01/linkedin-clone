b90ee8eeca5624607840f5c3e65eaa5fd31212a0
faabaf01 <farasya15@gmail.com>
2023-06-14 01:07:30 +0800

    added homepage header

--- src/App.js
+++ src/App.js
@@ -2,14 +2,25 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Login from "./components/Login";
 import ErrorPage from "./components/ErrorPage";
+import Home from "./components/Home";
+import Header from "./components/Header";
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Routes>
-          <Route exact path="/" element={<Login />} />
+          <Route path="/" element={<Login />} />
           <Route path="*" element={<ErrorPage />} />
+          <Route
+            path="/home"
+            element={
+              <>
+                <Header />
+                <Home />
+              </>
+            }
+          />
         </Routes>
       </Router>
     </div>
--- src/components/ErrorPage.js
+++ src/components/ErrorPage.js
@@ -1,7 +1,30 @@
 import React from "react";
+import styled from "styled-components";
 
 function ErrorPage() {
-  return <div>Error! Page not found.</div>;
+  return (
+    <Error>
+      <h1>Sorry! The page that you are trying to visit does not exist.</h1>
+    </Error>
+  );
 }
+
+const Error = styled.div`
+  width: 100%;
+  h1 {
+    padding-bottom: 0;
+    width: 55%;
+    font-size: 56px;
+    color: #2977c9;
+    font-weight: 200;
+    line-height: 70px;
+    @media (max-width: 768px) {
+      text-align: center;
+      font-size: 20px;
+      width: 100%;
+      line-height: 2;
+    }
+  }
+`;
 
 export default ErrorPage;
