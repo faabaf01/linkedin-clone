eae94c63454fab1b857756e743b0756d65e373d0
faabaf01 <farasya15@gmail.com>
2023-06-15 13:11:41 +0800

    added left right main components

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,7 @@
 import styled from "styled-components";
+import LeftSide from "./LeftSide";
+import Main from "./Main";
+import RightSide from "./RightSide";
 
 const Home = (props) => {
   return (
@@ -13,9 +16,9 @@ const Home = (props) => {
         </p>
       </Section>
       <Layout>
-        <div>Left Side</div>
-        <div>Main</div>
-        <div>Right Side</div>
+        <LeftSide />
+        <Main />
+        <RightSide />
       </Layout>
     </Container>
   );
