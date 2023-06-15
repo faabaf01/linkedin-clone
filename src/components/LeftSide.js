05451830af47bd4e807ae877fe1651a269f13a99
faabaf01 <farasya15@gmail.com>
2023-06-15 16:28:43 +0800

    fixed leftside

--- src/components/LeftSide.js
+++ src/components/LeftSide.js
@@ -26,9 +26,28 @@ const LeftSide = () => {
         <Item>
           <span>
             <img src="/images/item-icon.svg" alt="" />
+            My Items
           </span>
         </Item>
       </ArtCard>
+
+      <CommunityCard>
+        <a>
+          <span>Groups</span>
+        </a>
+        <a>
+          <span>
+            Events
+            <img src="/images/plus-icon.svg" alt="" />
+          </span>
+        </a>
+        <a>
+          <span>Follow Hashtags</span>
+        </a>
+        <a>
+          <span>Discover more</span>
+        </a>
+      </CommunityCard>
     </Container>
   );
 };
@@ -96,7 +115,7 @@ const AddPhotoText = styled.div`
 `;
 
 const Widget = styled.div`
-  border-radius: 1px solid rgba(0, 0, 0, 0.15);
+  border-bottom: 1px solid rgba(0, 0, 0, 0.15);
   padding-top: 12px;
   padding-bottom: 12px;
 
@@ -127,8 +146,62 @@ const Widget = styled.div`
       }
     }
   }
+
+  svg {
+    color: rgba(0, 0, 0, 1);
+  }
 `;
 
-const Item = styled.div``;
+const Item = styled.a`
+  border-color: rgba(0, 0, 0, 0.8);
+  text-align: left;
+  padding: 12px;
+  font-size: 12px;
+  display: block;
+  span {
+    display: flex;
+    align-items: center;
+    color: rgba(0, 0, 0, 1);
+    svg {
+      color: rgba(0, 0, 0, 0.6);
+    }
+  }
+
+  &:hover {
+    background-color: rgba(0, 0, 0, 0.08);
+  }
+`;
+
+const CommunityCard = styled(ArtCard)`
+  padding: 8px 0 0;
+  text-align: left;
+  display: flex;
+  flex-direction: column;
+  a {
+    color: black;
+    padding: 4px 12px 4px 12px;
+    font-size: 12px;
+
+    &:hover {
+      color: #0a66c2;
+    }
+
+    span {
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+    }
+
+    &:last-child {
+      color: rgba(0, 0, 0, 0.6);
+      text-decoration: none;
+      border-top: 1px solid#d6cec2;
+      padding: 12px;
+      &:hover {
+        background-color: rgba(0, 0, 0, 0.08);
+      }
+    }
+  }
+`;
 
 export default LeftSide;
