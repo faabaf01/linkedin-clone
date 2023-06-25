f8d133c63f1bd5f5cafab4657469c9f0b38cada2
faabaf01 <farasya15@gmail.com>
2023-06-25 22:09:45 +0800

    added usereducer

---
+++ src/reducers/userReducer.js
@@ -0,0 +1,14 @@
+//Reducer is stateUpdater
+
+const INITIAL_STATE = {
+  user: null,
+};
+
+const userReducer = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+export default userReducer;
